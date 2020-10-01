4abe52c3bf2185a609af71e99753b36a801f2f78
GlueSanchez <60786538+GlueSanchez@users.noreply.github.com>
2020-10-01 14:09:35 +0300

    create index.js

---
+++ src/index.js
@@ -0,0 +1,31 @@
+exports.min = function min(array) {
+    if (!array || array.length === 0) {
+        return 0;
+    } else {
+        array.sort(function (a, b) {
+            return a - b;
+        });
+        return array[0];
+    }
+}
+
+exports.max = function max(array) {
+    if (!array || array.length === 0) {
+        return 0;
+    } else {
+        array.sort(function (a, b) {
+            return a - b;
+        });
+        return array[array.length - 1];
+    }
+
+}
+
+exports.avg = function avg(array) {
+    if (!array || array.length === 0) {
+        return 0;
+    } else {
+      return array.reduce((acc, cur) => (acc + cur), 0) / array.length;
+
+    }
+}
